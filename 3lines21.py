743407594d04b87f0fcb59a519f54ce7009b15f4
arcboundavenger <40199645+arcboundavenger@users.noreply.github.com>
2018-07-03 18:29:24 +0800

    1

--- 3lines21.py
+++ 3lines21.py
@@ -1,7 +1,7 @@
 #-*- coding:utf-8 -*-
 
 import re
-
+import time
 infp = open('Result_Youtube_Reformatted.txt', "r")
 outfp = open('Result_Youtube_Edited.txt', 'w')
 lines = infp.readlines()
@@ -12,9 +12,17 @@ for i in range(0,len(lines)):
 # print lines
 lines2 = []
 i=0
-for j in range(0, len(lines)-3, 3):
-    addthing = lines[j] + lines[j+1] + lines[j+2]
+
+list1 = ['jacksepticeye', 'markiplierGAME', 'TheDiamondMinecart', 'theRadBrad', 'MiniLaddd']
+dict1 = {'jacksepticeye':'1953万', 'markiplierGAME':'2099万', 'TheDiamondMinecart':'1943万', 'MiniLaddd':'482万', 'theRadBrad':'900万'}
+for j in range(0, len(lines)-6, 3):
+    addthing = lines[j] + lines[j+1] + lines[j+2] + list1[i] + '@@@' + dict1[list1[i]]
     lines2.append(addthing+'\n')
+    print addthing
+    if (re.search('周前', lines[j+2]) != None and re.search('周前', lines[j+5]) == None and re.search('月前', lines[j+5]) == None):
+        i=i+1
+    elif (re.search('月前', lines[j+2]) != None and re.search('月前', lines[j+5]) == None):
+        i=i+1
 
 
-outfp.writelines(lines2)
+outfp.writelines(lines2[0:143])
