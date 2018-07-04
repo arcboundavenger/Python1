16a428fcbb19383e743df582779968400fcdac93
arcboundavenger <40199645+arcboundavenger@users.noreply.github.com>
2018-07-04 17:09:50 +0800

    1

--- csv2xlsx.py
+++ csv2xlsx.py
@@ -1,7 +1,6 @@
 #-*-coding:utf-8 -*-
 import pandas as pd
 from datetime import datetime
-import openpyxl
 def csv_to_xlsx_pd():
     csv = pd.read_csv('Result_Youtube_Edited_'+ datetime.now().date().strftime('%Y%m%d')+'.csv', encoding='utf-8')
     csv.to_excel('Result_Youtube_Edited_'+ datetime.now().date().strftime('%Y%m%d')+'.xlsx', sheet_name='data')
