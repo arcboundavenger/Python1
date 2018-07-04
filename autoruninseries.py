76e2cb7038598e66e77ca3dc3655f1dfeacb5193
arcboundavenger <40199645+arcboundavenger@users.noreply.github.com>
2018-07-04 18:37:40 +0800

    1

--- autoruninseries.py
+++ autoruninseries.py
@@ -1,12 +1,12 @@
 import os
 import time
 os.system("python ./youtuberetrieve.py")
-time.sleep(5)
+time.sleep(3)
 os.system("python ./deleteblanklines.py")
-time.sleep(1)
+time.sleep(2)
 os.system("python ./3lines21.py")
-time.sleep(1)
+time.sleep(2)
 os.system("python ./text2csv.py")
-time.sleep(1)
+time.sleep(2)
 os.system("python ./csv2xlsx.py")
 
