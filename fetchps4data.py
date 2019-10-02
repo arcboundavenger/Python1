0da9ce2eeaf6b5cbc6cce68fbdf3b697aae1edc3
arcboundavenger <arcboundavenger@gmail.com>
2019-10-02 19:45:47 +0800

    new project

--- fetchps4data.py
+++ fetchps4data.py
@@ -9,47 +9,41 @@ import pymouse,pykeyboard,os,sys
 from pymouse import *
 from pykeyboard import PyKeyboard
 import xlrd
+import xlsxwriter
 
-# 爬虫函数
-def crawl(url):
-    driver = webdriver.Firefox(executable_path='C:/Python27/geckodriver')
-    driver.get(url)
-    textarea = driver.find_elements_by_xpath("//input[@type='search']")
-    #content = driver.find_elements_by_xpath("//div[@class='content clearfix']/div[@class='small-item fakeDanmu-item']")
-    #content = driver.find_elements_by_xpath("//div[@class='h-basic-spacing']")
-    print (content)
-    # infofile.write(url + "\r\n")
-    js2 = "var q=document.getElementById('label_ps4').click()"
-    driver.execute_script(js2)
-    textarea = driver.find_elements_by_xpath("//div[@id='games_filter']//input[@type='search']")[0]
-    textarea.send_keys('asdkjhf')
-    for tag in content:
-        #print (tag.text)
-        infofile.write(tag.text + "\r\n")
-        print (' ')
-    time.sleep(5)
-    driver.quit()
-    # 主函数
+
 
 
 if __name__ == '__main__':
+
     ExcelFile = xlrd.open_workbook(r'C:/Python27/sheet1.xlsx')
     sheet = ExcelFile.sheet_by_index(0)
     cols = sheet.col_values(0)
     dels cols[0]
+    newcols = []
+    #读xlsx表
 
-    infofile = codecs.open("Result_PS4.txt", 'w', 'utf-8')
+    workbook = xlsxwriter.Workbook('result_ps4_Exclusive.xlsx')
+    worksheet = workbook.add_worksheet(u'sheet1')
+    #生成空表
 
-    i = 0
-    url=''
+    driver = webdriver.Firefox(executable_path='C:/Python27/geckodriver')
+    url = 'http://gamstat.com/games'
+    driver.get(url)
+    #打开网页
 
-    list1 = [2728123]
-    while i < len(list1):
-        url = 'https://gamstat.com/games/'
-        print (url)
-        crawl(url)
-        infofile.write("\r\n\r\n\r\n")
-        i = i + 1
+    js2 = "var q=document.getElementById('label_ps4').click()"
+    driver.execute_script(js2)
+    #点击PS4字样
 
-    infofile.close()
+    textarea = driver.find_elements_by_xpath("//div[@id='games_filter']//input[@type='search']")[0]
+    #找到想要的文字输入框
+
+
+    for i in cols:
+        textarea.send_keys(i)
+        newcols[i] = driver.find_element_by_xpath('//tbody/tr[1]/td[5]').text
+        time.sleep(5)
+
+    worksheet.write_column('A1', newcols)
 
