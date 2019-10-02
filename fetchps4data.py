141b493784b1df2a5cd886048ce8897f0940548e
arcboundavenger <arcboundavenger@gmail.com>
2019-10-03 00:40:48 +0800

    new project

--- fetchps4data.py
+++ fetchps4data.py
@@ -18,32 +18,40 @@ if __name__ == '__main__':
 
     driver = webdriver.Firefox(executable_path='C:/Python27/geckodriver')
     url = 'http://gamstat.com/games'
-    driver.get(url)
-    #打开网页
 
-    for i in cols:
-        time.sleep(5)
+    driver.set_page_load_timeout(600)
+    try:
+        driver.get(url)
+        time.sleep(2)
         js2 = "var q=document.getElementById('label_ps4').click()"
         driver.execute_script(js2)
-        #点击PS4字样
         time.sleep(2)
-        textarea = driver.find_elements_by_xpath("//div[@id='games_filter']//input[@type='search']")[0]
-        #找到想要的文字输入框
-        textarea.send_keys(i)
-        time.sleep(7)
-        #输入文字
-        try:
-            content = driver.find_element_by_xpath('//tbody/tr[1]/td[5]').text
-            print (content)
-            newcols.append(content)
-        except:
-            pass
-            content = 'N/A'
-            newcols.append(content)
-            print (content)
-        #一个有可能遇到错误的爬取循环
-        textarea.clear()
-        #清空文本
+        # 点击PS4字样
+        #打开网页
+        for i in cols:
+            textarea = driver.find_elements_by_xpath("//div[@id='games_filter']//input[@type='search']")[0]
+            #找到想要的文字输入框
+            textarea.send_keys(' ' + i)
+            time.sleep(5)
+            #输入文字
+
+            try:
+                content = driver.find_element_by_xpath('//tbody/tr[1]/td[5]').text
+                print (content)
+                newcols.append(content)
+            except:
+                pass
+                content = 'N/A'
+                newcols.append(content)
+                print (content)
+            #一个有可能遇到错误的爬取循环
+            textarea.clear()
+            time.sleep(5)
+            #删除文字
+
+    except:
+        print('timeout')
+        pass
 
 
     worksheet.write_column('A1', newcols)
