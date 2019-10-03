5c33a109c508d3f782c4e328640a09dfaefb3f8b
arcboundavenger <arcboundavenger@gmail.com>
2019-10-04 00:18:23 +0800

    new project

--- fetchps4data.py
+++ fetchps4data.py
@@ -23,31 +23,34 @@ if __name__ == '__main__':
     try:
         driver.get(url)
         time.sleep(2)
+        # 打开网页
         js2 = "var q=document.getElementById('label_ps4').click()"
         driver.execute_script(js2)
         time.sleep(2)
         # 点击PS4字样
-        #打开网页
+
         for i in cols:
             textarea = driver.find_elements_by_xpath("//div[@id='games_filter']//input[@type='search']")[0]
-            #找到想要的文字输入框
+            # 找到想要的文字输入框
             textarea.send_keys(' ' + i)
             time.sleep(5)
-            #输入文字
+            # 输入文字
 
             try:
                 content = driver.find_element_by_xpath('//tbody/tr[1]/td[5]').text
                 print (content)
                 newcols.append(content)
+                # 一个有可能遇到错误的爬取循环
             except:
                 pass
                 content = 'N/A'
                 newcols.append(content)
                 print (content)
-            #一个有可能遇到错误的爬取循环
+                # 如果找不到则打N/A
+
             textarea.clear()
             time.sleep(5)
-            #删除文字
+            # 删除文字
 
     except:
         print('timeout')
