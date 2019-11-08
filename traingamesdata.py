0dc8c93feea36a82376775f5e8001629282e8a9a
arcboundavenger <arcboundavenger@gmail.com>
2019-11-08 17:01:41 +0800

    update lol

--- traingamesdata.py
+++ traingamesdata.py
@@ -8,35 +8,20 @@ from matplotlib import pyplot as plt
 from sklearn.metrics import precision_score, recall_score, accuracy_score
 from sklearn.model_selection import GridSearchCV
 
-s = pd.read_csv('GamesalesdataV3.csv')
+#读游戏的csv表
+s = pd.read_csv(r'C:\Users\Administrator\PycharmProjects\Python1\GamesalesdataV3.csv')
 X = s.values
-s1 = pd.read_csv('GamessalesTarget.csv')
+s1 = pd.read_csv(r'C:\Users\Administrator\PycharmProjects\Python1\GamessalesTarget.csv')
 y = s1.values
 
-
-
-
-
-
-
-
-# In[55]:
-
-
+#鸢尾花：
 # iris = load_iris()
 # X = iris.data
 # y = iris.target
-print ('type X')
 print(type(X))
-print ('type Y')
 print(type(y))
 
-
-
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-
-
 
 print ('X_train, Y_train')
 print(X_train, y_train)
@@ -44,13 +29,12 @@ print(X_train, y_train)
 
 params = {
     'eta': 0.01,
-    'n estimators':620,
+    'n estimators':900,
     'learning rate': 0.01,
-    'max_depth': 3,
+    'max_depth': 5,
     'objective': 'multi:softprob',
-    'gamma': 0.02,
-    'max_depth': 3,
-    'lambda': 1,
+    'gamma': 0.04,
+    'lambda': 10,
     'alpha':0,
     'subsample': 0.9,
     'colsample_bytree': 0.8,
@@ -61,35 +45,18 @@ params = {
     'num_class': 6
     }
 
-# In[64]:
-
-
 plst = params.items()
-
-
-# In[65]:
-
-print('params:')
 print(params)
-print('plst:')
 print(plst)
-
-
-# In[68]:
-
-
 
 dtrain = xgb.DMatrix(X_train, y_train)
 
 
-num_rounds = 500
+num_rounds = 900
 model = xgb.train(plst, dtrain, num_rounds)
-
-
 
 dtest = xgb.DMatrix(X_test)
 ans = model.predict(dtest)
-print('ans:')
 print(ans)
 
 plot_importance(model)
@@ -102,21 +69,20 @@ print("Recall = {}".format(recall_score(y_test, best_preds, average='macro')))
 print("Accuracy = {}".format(accuracy_score(y_test, best_preds)))
 plt.show()
 
-#
-
+# # 下面是GridSearch调参：
 # xgb_model = xgb.XGBClassifier(objective="multi:softprob", nthread=-1, num_class=4, seed=1000)
 #
 # optimized_GBM = GridSearchCV(
 #     xgb_model,
 #     {
-#         'n_estimators': [620],
-#         'max_depth': [3],
+#         'n_estimators': [900],
+#         'max_depth': [5],
 #         'min_child_weight': [1],
-#         'gamma': [0.02],
+#         'gamma': [0.04],
 #         'subsample': [0.9],
 #         'colsample_bytree': [0.8],
-#         'reg_lambda': [1],
-#         'reg_alpha': [0],
+#         'reg_lambda': [10.0],
+#         'reg_alpha': [0.0],
 #         'eta': [0.01],
 #         'scale_pos_weight': [0.1],
 #         'learning_rate': [0.01]
