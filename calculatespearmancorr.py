56f5e0c8962a9b76ac06c302c2289e7e912b5387
arcboundavenger <arcboundavenger@gmail.com>
2021-09-13 18:40:07 +0800

    2021.9.13

--- calculatespearmancorr.py
+++ calculatespearmancorr.py
@@ -4,7 +4,6 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from scipy import stats
 import palettable
 
 
@@ -14,8 +13,7 @@ train = pd.read_csv('forspearman.csv')
 dcorr = train.corr(method='spearman')#默认为'pearson'检验，可选'kendall','spearman'
 print(dcorr)
 plt.figure()
-plt.title('Spearman Correlation of Features (Dropped Empty Values)', fontsize = 14)
-# colormap = plt.cm.viridis
+plt.title('Spearman Correlation of Features', fontsize = 12)
 
 extreme_1 = 0.4  # show with a star
 extreme_2 = 0.7  # show with a second star
@@ -25,9 +23,12 @@ annot = [[f"{val:.2f}"
           + ('' if abs(val) < extreme_2 else '★')  # add an extra star if abs(val) >= extreme_2
           + ('' if abs(val) < extreme_3 else '★')  # add yet an extra star if abs(val) >= extreme_3
           for val in row] for row in dcorr.to_numpy()]
-
-sns.heatmap(dcorr,cmap='YlOrRd',
-            linewidths=0.1,vmin=-1, vmax=1.0 ,fmt="", square=True, annot=annot,annot_kws={'size':11,'weight':'bold', 'color':'white'},mask=np.triu(np.ones_like(dcorr,dtype=np.bool)))
+sns.heatmap(data=dcorr,
+            cmap=plt.cm.YlOrRd,
+            linewidths=0.1, vmin=-1, vmax=1.0 ,fmt="", square=True, annot=annot,annot_kws={'size':11,'weight':'bold', 'color':'white'},mask=np.triu(np.ones_like(dcorr,dtype=np.bool)),
+            xticklabels=True, yticklabels=True)
+mng = plt.get_current_fig_manager()
+mng.resize(*mng.window.maxsize())
 plt.xticks(fontsize = 11)
 plt.yticks(fontsize = 11)
 plt.show()
